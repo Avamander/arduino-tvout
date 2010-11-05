f8b52495493313b6822733c6df9132b8276282ff
Myles Metzler <mdmetzle@gmail.com>
2010-11-05 16:00:06 -0400

    fixed the 3x5 font renamed it to 4x6 to better reflect what it actually is.

--- TVoutfonts/font3x5.h
+++
@@ -1,7 +0,0 @@
-#ifndef FONT3X5_h
-#define FONT3X5_h
-#include <avr/pgmspace.h>
-
-extern const unsigned char font3x5[];
-
-#endif
--- TVoutfonts/fontALL.h
+++ TVoutfonts/fontALL.h
@@ -1,7 +1,7 @@
 #ifndef FONTALL_H
 #define FONTALL_H
 
-#include "font3x5.h"
+#include "font4x6.h"
 #include "font6x8.h"
 #include "font8x8.h"
 #include "font8x8ext.h"
